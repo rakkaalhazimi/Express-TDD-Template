dd1df0346206c7a1532abf16ed776717c3ed9432
Rakka Alhazimi <rakkakeren@gmail.com>
2026-09-09 15:22:29 +0700

    changed: line spacing in auth.controller

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import type { Request, Response } from "express";
+import type { Request, Response } from 'express';
 import { type TokenPayload } from 'google-auth-library';
 
 import { createAuthService } from './auth.service.js';
@@ -20,36 +20,32 @@ export function createAuthController(db: Services) {
       const { username, password } = req.body;
       const loginResponse = await authService.login(username, password);
       user = loginResponse.data;
-      
     } catch (e) {
       const response = await handleError(e, 'Login failed');
       return res.status(response.status).send(response);
     }
-    
-    // Generate token
+
     try {
       const response = await authService.genereateUserToken(user.id);
       return res.status(response.status).send(response);
-      
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Create JWT Failed');
       return res.status(response.status).send(response);
     }
   });
-
+  
 
   AuthController.post('/register', async (req: Request, res: Response) => {
     try {
       const { username, password, confirmPassword } = req.body;
       const response = await authService.register(username, password, confirmPassword);
       return res.status(response.status).send(response);
-
     } catch (e) {
       const response = await handleError(e, 'Register failed');
       return res.status(response.status).send(response);
     }
   });
-
+  
 
   AuthController.get('/google', async (req: Request, res: Response) => {
     const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
@@ -61,42 +57,36 @@ export function createAuthController(db: Services) {
     }).toString();
     res.redirect(url.toString());
   });
-
+  
 
   AuthController.get('/google/callback', async (req: Request, res: Response) => {
-    // Authorize request from google
     let payload: TokenPayload | null = null;
     try {
       payload = await authService.authorizeGoogle(req);
-      
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Google Authorization failed');
       return res.status(response.status).send(response);
     }
-    
-    // Login / Register with google id
+
     let userAuth: any = null;
     try {
       const googleResponse = await authService.registerByGoogle(payload!.sub, payload!.email!);
       userAuth = googleResponse.data;
-      
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Login/Register with google failed');
       return res.status(response.status).send(response);
     }
 
-    // Generate token
     try {
       const response = await authService.genereateUserToken(userAuth.user?.id);
       return res.status(response.status).send(response);
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Create JWT failed');
       return res.status(response.status).send(response);
     }
   });
   
-  
+
   AuthController.get('/github', async (req: Request, res: Response) => {
     const url = new URL('https://github.com/login/oauth/authorize');
     url.search = new URLSearchParams({
@@ -106,41 +96,36 @@ export function createAuthController(db: Services) {
     }).toString();
     res.redirect(url.toString());
   });
-
+  
 
   AuthController.get('/github/callback', async (req: Request, res: Response) => {
-    // Authorize request from github
     let payload: any = null;
     try {
       payload = await authService.authorizeGithub(req);
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Github Authorization failed');
       return res.status(response.status).send(response);
     }
 
-    // Login / Register with github id
     let userAuth: any = null;
     try {
       const { data } = await authService.registerByGithub(String(payload.id), payload.email || payload.login);
       userAuth = data;
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Login/Register with github failed');
       return res.status(response.status).send(response);
     }
-    
-    // Generate token
+
     try {
       const response = await authService.genereateUserToken(userAuth.user?.id);
       return res.status(response.status).send(response);
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Create JWT failed');
       return res.status(response.status).send(response);
     }
   });
   
-  
+
   AuthController.get('/discord', async (req: Request, res: Response) => {
     const url = new URL('https://discord.com/oauth2/authorize');
     url.search = new URLSearchParams({
@@ -152,73 +137,63 @@ export function createAuthController(db: Services) {
     res.redirect(url.toString());
   });
   
-  
+
   AuthController.get('/discord/callback', async (req: Request, res: Response) => {
-    // Authorize request from discord
     let payload: any = null;
     try {
       payload = await authService.authorizeDiscord(req);
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Discord Authorization failed');
       return res.status(response.status).send(response);
     }
-    
-    // Login / Register with discord id
+
     let userAuth: any = null;
     try {
       const discordResponse = await authService.registerByDiscord(String(payload.id), payload.username);
       userAuth = discordResponse.data;
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Login/Register with discord failed');
       return res.status(response.status).send(response);
     }
 
-    // Generate token
     try {
       const response = await authService.genereateUserToken(userAuth.user?.id);
       return res.status(response.status).send(response);
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Create JWT failed');
       return res.status(response.status).send(response);
     }
   });
   
-  
+
   AuthController.get('/microsoft', async (req: Request, res: Response) => {
     const url = await authService.getMSAuthUrl();
     res.redirect(url);
   });
   
-  
+
   AuthController.get('/microsoft/callback', async (req: Request, res: Response) => {
-    // Authorize request from microsoft
     let payload: any = null;
     try {
       payload = await authService.authorizeMicrosoft(req);
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Microsoft Authorization failed');
       return res.status(response.status).send(response);
     }
-    
-    // Login / Register with microsoft id
+
     let userAuth: any = null;
     try {
       const microsoftResponse = await authService.registerByMicrosoft(String(payload.oid), payload.userPrincipalName);
       userAuth = microsoftResponse.data;
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Login/Register with microsoft failed');
       return res.status(response.status).send(response);
     }
 
-    // Generate token
     try {
       const response = await authService.genereateUserToken(userAuth.user?.id);
       return res.status(response.status).send(response);
-
-    } catch(e) {
+    } catch (e) {
       const response = await handleError(e, 'Create JWT failed');
       return res.status(response.status).send(response);
     }
