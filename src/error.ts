f5c0e34dd27fa6d4868227e136052e8490142b96
Rakka Alhazimi <rakkakeren@gmail.com>
2026-09-19 15:21:33 +0700

    rm: handleError

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,6 @@
 import { randomUUID } from 'node:crypto';
 
 import { StatusCodes } from 'http-status-codes';
-import { type Response } from '@/response.js';
 
 
 
@@ -36,20 +35,4 @@ export function createAppError(error: unknown, message: string): AppError {
 		cause: error,
 	});
 	return appError;
-}
-
-
-export async function handleError(
-	e: unknown,
-	message: string,
-	status: number | null = null
-): Promise<Response> {
-	const error = e as Error;
-	console.error(error.stack, error.message);
-
-	return {
-		message,
-		status: status ?? StatusCodes.INTERNAL_SERVER_ERROR,
-		data: error.message,
-	};
 }
